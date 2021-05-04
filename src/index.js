d6d2d02da79586b79f900ba1a6676cbf15bb9c1c
Kiryl <Kiryl7St@gmail.com>
2021-05-04 19:02:56 +0300

    refactoring code
    very good

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const users = [{
 },
 {
     name: 'name2',
-    surname: 'surname2',
+
     patronymic: 'patronymic2',
     age: 34
 },
@@ -36,18 +36,17 @@ const byField = (field) => {
 }
 
 class Table{
-    constructor(table, count, cnt, cnt_page) {
+    constructor(table, count, cnt, cnt_page, tempUsers) {
         this.table = table;
         this.count = count;
         this.cnt = cnt;
         this.cnt_page = cnt_page;
-        this.a = 5
+        this.tempUsers = tempUsers;
     }
 
     renderAll() {
         this.renderTH()
         this.renderTD()
-        this.displayTR()
         this.renderPage()
     }
 
@@ -59,20 +58,11 @@ class Table{
         }
     }
 
-    renderTH() {
-        let noUniqueField = [] // добавление всех полей в массив для заполнения шапки
-        for (let i = 0; i < users.length; i++) {
-            let temp = Object.getOwnPropertyNames(users[i])
-            noUniqueField.push(temp)
-        }
-        let flatNoUniqueField = noUniqueField.flat(Infinity)
-        let uniqueField = [] // сортировка по уникальности
-        for (let str of flatNoUniqueField) {
-            if (!uniqueField.includes(str)) {
-                uniqueField.push(str)
-            }
-        }
-        for (let data in uniqueField) { //заполнение шапки
+    renderTH() { //заполнение шапки
+        let uniqueField = Object.keys(users.reduce(function(result, obj) {
+            return Object.assign(result, obj);
+          }, {}))
+        for (let data in uniqueField) {
             let th = document.createElement('div')
             th.classList.add('d-th')
             th.innerHTML = uniqueField[data]
@@ -88,113 +78,62 @@ class Table{
         }
         document.getElementById(field.target.innerHTML).style.color = 'red'
         users.sort(byField(field.target.innerHTML))
-        users.forEach((obj, i) => {
-            let tr = document.getElementById('tr' + i)
-            this.table.removeChild(tr)
-        })
         this.renderTD()
-        this.displayTR()
     }
 
 
     renderTD() { // создание и заполнение ячеек таблицы
+        this.tableDisplay()
         let k = 0
         let th = document.querySelectorAll('.d-th')
-        users.forEach((obj, i) => {
-            let p = 0
+        if(document.querySelector('.d-tr')) {
+            let trLenght = document.querySelectorAll('.d-tr').length
+            for(let x = 0; x < trLenght; x++) {
+                let tr = document.getElementById('tr' + x)
+                this.table.removeChild(tr)
+            }
+        }
+        this.tempUsers.forEach((obj, i) => {
             let tr = document.createElement('div');
             tr.classList.add('d-tr')
             tr.id = 'tr' + i
-            for (let data in obj) {
-                let thId = th[p].id
-                if (thId === data) {
-                    let td = document.createElement('div')
-                    td.classList.add('d-td')
-                    td.innerHTML = obj[data]
-                    td.id = 'td' + k
-                    k++
+            let p = 0
+            for (let y = 0; y < th.length; y++) {
+                let thId = th[y].id
+                let obj_key = Object.keys(obj)
+                let td = document.createElement('div')
+                td.classList.add('d-td')
+                td.id = 'td' + k
+                if(thId == obj_key[p]) {
+                    td.innerHTML = obj[thId]
                     tr.appendChild(td)
+                    p++
                 }
-                else if (thId !== data) {
-                    for (p; p < th.length; p++) {
-                        thId = th[p].id
-                        if (thId === data) {
-                            let td = document.createElement('div')
-                            td.classList.add('d-td')
-                            td.innerHTML = obj[data]
-                            td.id = 'td' + k
-                            k++
-                            tr.appendChild(td)
-                            break
-                        }
-                        else {
-                            let td = document.createElement('div')
-                            td.classList.add('d-td')
-                            td.innerHTML = ('')
-                            tr.appendChild(td)
-                        }
+                    else {
+                        td.innerHTML = ''
+                        tr.appendChild(td)
                     }
-                }
-                p++
+                k++
             }
-            this.table.appendChild(tr);
+            this.table.appendChild(tr)
         })
     }
 
     renderPage() {
         const paginator = document.querySelector(".paginator"); //выводим список страниц
-        let page = "";
+        let page = ""
         for (let i = 0; i < this.cnt_page; i++) {
-            page += "<span data-page=" + i * cnt + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";
+            page += "<span data-page=" + i * this.cnt + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>"
         }
-        paginator.innerHTML = page;
+        paginator.innerHTML = page
     }
 
     more() { //добавление отображаемых строк на одной странице
         if (this.cnt >= users.length) return
         this.cnt += 1
-        this.cnt_page = Math.ceil(count / cnt)
+        this.cnt_page = Math.ceil(this.count / this.cnt)
+        this.renderTD()
         this.renderPage()
-        this.displayTR()
-    }
-
-    displayTR() { //выводим заданное количество строк
-        const div_dTR = document.querySelectorAll(".d-tr");
-        for (let i = 0; i < div_dTR.length; i++) {
-            if (i < this.cnt) div_dTR[i].style.display = "table-row";
-        }
-    }
-
-    displayOffTR() { //вызываем для перерисовки таблицы при уменьшении количества элементов
-        const div_dTR = document.querySelectorAll(".d-tr")
-        for (let i = this.cnt; i < div_dTR.length; i++) {
-            if (i >= this.cnt) div_dTR[i].style.display = "none"
-        }
-    }
-
-    pagination(event) { //листаем
-        const e = event || window.event;
-        const target = e.target;
-        const id = target.id;
-        const div_dTR = document.querySelectorAll(".d-tr");
-
-        if (target.tagName.toLowerCase() !== "span") return;
-
-        let data_page = +target.dataset.page;
-        main_page.classList.remove("paginator_active");
-        main_page = document.getElementById(id);
-        main_page.classList.add("paginator_active");
-        let j = 0;
-        for (let i = 0; i < div_dTR.length; i++) {
-            if (i !== data_page)
-                div_dTR[i].style.display = "none";
-
-        }
-        for (let i = data_page; i < div_dTR.length; i++) {
-            if (j >= this.cnt) break;
-            div_dTR[i].style.display = "table-row";
-            j++;
-        }
     }
 
     less() { //уменьшение отображаемых строк на одной странице
@@ -202,16 +141,45 @@ class Table{
         this.cnt -= 1
         this.cnt_page = Math.ceil(this.count / this.cnt)
         this.renderPage()
-        this.displayOffTR()
+        this.renderTD()
+    }
+
+    tableDisplay() { //копируем часть массива для рендера
+        let activePage = 1
+        let numActivePage = null
+        if(document.querySelector('.paginator_active')) activePage = document.querySelector('.paginator_active')
+        if(activePage == 1) numActivePage = 1
+            else numActivePage = activePage.textContent 
+        let startPos = (this.cnt * numActivePage) - this.cnt 
+        this.tempUsers = users.slice(startPos, startPos + this.cnt)
+        return tempUsers
+    }
+
+    pagination(event) { //листаем
+        const e = event || window.event
+        const target = e.target
+        const id = target.id
+        //const div_dTR = document.querySelectorAll(".d-tr")
+
+        if (target.tagName.toLowerCase() !== "span") return
+
+        //let data_page = +target.dataset.page
+        main_page.classList.remove("paginator_active")
+        main_page = document.getElementById(id)
+        main_page.classList.add("paginator_active")
+
+        this.renderTD()
     }
 }
 
+let tempUsers = []
 let table = document.getElementById('table');
 let count = users.length; //всего записей
 let cnt = 2 //сколько отображаем сначала
 let cnt_page = Math.ceil(count / cnt); //кол-во страниц
 
-const tableObj = new Table(table, count, cnt, cnt_page)
+const tableObj = new Table(table, count, 2, Math.ceil(count / cnt))
+
 
 tableObj.renderAll()
 tableObj.setListeners()
